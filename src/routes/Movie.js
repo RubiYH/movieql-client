59d4b979af3c485ec2e1f75799aa67dadfe13562
YH <imrubi5555@gmail.com>
2023-12-03 21:01:08 +0900

    Apollo Client

---
+++ src/routes/Movie.js
@@ -0,0 +1,28 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+      small_cover_image
+    }
+  }
+`;
+
+export default function Movie() {
+  const { id } = useParams();
+  const { data, loading } = useQuery(GET_MOVIE, {
+    variables: {
+      movieId: id,
+    },
+    // fetchPolicy: "cache-and-network",
+  });
+
+  console.log(data, loading);
+  if (loading) {
+    return <h1>Fetching movie...</h1>;
+  }
+  return <div>{data.movie.title}</div>;
+}
